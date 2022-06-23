47cb0100e2665d047a42daf20ab3bda6cde6050c
Jerit Baiju <jeritalumkal@gmail.com>
2022-06-23 15:49:39 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,23 @@
+/**
+ * variables
+ */
+public class variables {
+
+    public static void main(String[] args) {
+        // declaring variables
+        double principal;
+        double rate;
+        double interest;
+
+        // computation
+        principal = 17000;
+        rate = 0.7;
+        interest = principal * rate; //calculate
+
+        System.out.print("the interest is $");
+        System.out.println(interest);
+
+
+
+    }
+}
